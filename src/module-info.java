205c12cb5b2b2a20d39330382ae48e074f06626f
bomberman114 <mielmer016@gmail.com>
2022-07-17 21:01:55 +0900

    second2java

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Second2 {
+}
---
+++ src/second2/Second2java.java
@@ -0,0 +1,11 @@
+package second2;
+
+public class Second2java {
+
+	public static void main(String[] args) {
+		boolean isSunday = true;
+    System.out.println( isSunday );
+
+	}
+
+}
